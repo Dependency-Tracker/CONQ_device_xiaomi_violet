PixysOS-Devices/device_xiaomi_violet

violet: fingerprint: Uprev to v2.2
Change-Id: Ie4ace90e4d0819c12cb14361dc84ff393afe323d

device.mk
@@ -213,7 +213,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
+    android.hardware.biometrics.fingerprint@2.2-service.xiaomi_sm6150
 
 # FM
 PRODUCT_PACKAGES += \

Dependency-Tracker/CONQ_device_xiaomi_violet

device.mk
@@ -151,7 +151,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_violet
+    android.hardware.biometrics.fingerprint@2.2-service.xiaomi_violet
 
 # FM
 PRODUCT_PACKAGES += \